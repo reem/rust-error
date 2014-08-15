aa4a9f75ee424b474e866ebc90b9725d6a3b29ef
Jonathan Reem <jonathan.reem@gmail.com>
2014-08-15 13:39:34 -0700

    (feat) Converted to Marker types instead of &'static str.

--- src/lib.rs
+++ src/lib.rs
@@ -6,28 +6,31 @@
 
 extern crate convertible;
 
-use convertible::{Raw, Convertible};
+use convertible::Convertible;
+use std::intrinsics::TypeId;
 
 pub struct Phantom<T>;
 
-pub struct RawError {
-    pub kind: &'static str,
+pub struct RawError<Marker> {
     pub description: Option<&'static str>,
     pub details: Option<String>
 }
 
-impl RawError {
-    pub fn is<E: Error>(&self) -> bool { Error::is(self, Phantom::<E>) }
+impl<T: 'static> RawError<T> {
+    pub fn is<O: 'static, E: Error<O>>(&self) -> bool { TypeId::of::<T>() == TypeId::of::<O>() }
 }
 
-pub trait Error: Convertible<RawError> {
-    fn as_raw(&self) -> RawError;
-    fn is(raw: &RawError, _: Phantom<Self>) -> bool {
-        raw.to::<Self>().is_some()
+pub trait Error<T: 'static>: Convertible<RawError<T>> {
+    fn as_raw(&self) -> RawError<T>;
+
+    fn is<R: 'static>(&self) -> bool {
+        TypeId::of::<T>() == TypeId::of::<R>()
     }
+
+    fn marker(Phantom<Self>) -> T;
 }
 
-impl<E: Error> Convertible<E> for RawError {
-    fn convert(err: &E) -> Option<RawError> { Some(err.as_raw()) }
+impl<O: 'static, E: Error<O>> Convertible<E> for RawError<O> {
+    fn convert(err: &E) -> Option<RawError<O>> { Some(err.as_raw()) }
 }
 
