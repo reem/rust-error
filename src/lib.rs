40ea49ab1772bb15720c40926eabd120b79d824f
Jonathan Reem <jonathan.reem@gmail.com>
2014-08-15 14:44:34 -0700

    (refactor) Removed marker and simplified is types.

--- src/lib.rs
+++ src/lib.rs
@@ -8,27 +8,23 @@ extern crate convertible;
 
 use convertible::Convertible;
 use std::intrinsics::TypeId;
+use std::any::Any;
 
-pub struct Phantom<T>;
-
+#[deriving(Show)]
 pub struct RawError<Marker> {
     pub description: Option<&'static str>,
     pub details: Option<String>,
-    pub extensions: Box<Any>
+    pub extensions: Option<Box<Any>>
 }
 
 impl<T: 'static> RawError<T> {
-    pub fn is<O: 'static, E: Error<O>>(&self) -> bool { TypeId::of::<T>() == TypeId::of::<O>() }
+    pub fn is<R: 'static>(&self) -> bool { TypeId::of::<T>() == TypeId::of::<R>() }
 }
 
 pub trait Error<T: 'static>: Convertible<RawError<T>> {
     fn as_raw(&self) -> RawError<T>;
 
-    fn is<R: 'static>(&self) -> bool {
-        TypeId::of::<T>() == TypeId::of::<R>()
-    }
-
-    fn marker(Phantom<Self>) -> T;
+    fn is<R: 'static>(&self) -> bool { TypeId::of::<T>() == TypeId::of::<R>() }
 }
 
 impl<O: 'static, E: Error<O>> Convertible<E> for RawError<O> {
