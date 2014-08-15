87cda343b18d78d1bf1c4a16539032ee6f6138af
Jonathan Reem <jonathan.reem@gmail.com>
2014-08-15 14:57:44 -0700

    (test) Added test for generic error handling.

--- src/lib.rs
+++ src/lib.rs
@@ -82,5 +82,22 @@ mod test {
         let parse = ParseError { location: 10u };
         assert_eq!(parse.to::<RawError<ParseErrorMarker>>().unwrap().description, Some("Parse-Error"));
     }
+
+    #[test] fn test_generic() {
+        fn produce_parse_error() -> RawError<ParseErrorMarker> {
+            RawError {
+                description: None,
+                details: None,
+                extensions: None
+            }
+        }
+
+        fn generic_handler<T: 'static>(raw: RawError<T>) {
+            let parse = raw.to::<ParseError>().unwrap();
+            assert_eq!(parse, ParseError { location: 7u });
+        }
+
+        generic_handler(produce_parse_error())
+    }
 }
 
