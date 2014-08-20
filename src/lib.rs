264047a858f2a322ae019255780f4a8eb34a183d
Jonathan Reem <jonathan.reem@gmail.com>
2014-08-19 17:31:10 -0700

    (test) Updated tests and example.

--- src/lib.rs
+++ src/lib.rs
@@ -61,71 +61,25 @@ impl<T: 'static> ErrorPrivate for T {
 
 #[cfg(test)]
 mod test {
-    use super::{RawError, Error};
-    use convertible::{Convertible, Raw};
-
-    #[deriving(Show)]
-    pub struct ParseErrorMarker;
+    use super::{Error, ErrorRefExt};
 
     #[deriving(Show, PartialEq)]
     pub struct ParseError {
         location: uint,
     }
 
-    impl<T: 'static> Convertible<RawError<T>> for ParseError {
-        fn convert(err: &RawError<T>) -> Option<ParseError> {
-            if err.is::<ParseErrorMarker>() {
-                Some(ParseError {
-                    location: 7u,
-                })
-            } else {
-                None
-            }
-        }
-    }
-
-    impl Error<ParseErrorMarker> for ParseError {
-        fn as_raw(&self) -> RawError<ParseErrorMarker> {
-            RawError {
-                description: Some("Parse-Error"),
-                details: None,
-                extensions: None,
-                cause: None
-            }
-        }
-    }
-
-    #[test] fn test_convert_from_raw() {
-        let raw: RawError<ParseErrorMarker> = RawError {
-            description: None,
-            details: None,
-            extensions: None,
-            cause: None
-        };
-
-        let parse = raw.to::<ParseError>().unwrap();
-
-        assert_eq!(parse, ParseError { location: 7u });
-    }
-
-    #[test] fn test_convert_to_raw() {
-        let parse = ParseError { location: 10u };
-        assert_eq!(parse.to::<RawError<ParseErrorMarker>>().unwrap().description, Some("Parse-Error"));
+    impl Error for ParseError {
+        fn name(&self) -> &'static str { "Parse Error" }
     }
 
     #[test] fn test_generic() {
-        fn produce_parse_error() -> RawError<ParseErrorMarker> {
-            RawError {
-                description: None,
-                details: None,
-                extensions: None,
-                cause: None
-            }
+        fn produce_parse_error() -> Box<Error> {
+            ParseError { location: 7u }.abstract()
         }
 
-        fn generic_handler<T: 'static>(raw: RawError<T>) {
-            let parse = raw.to::<ParseError>().unwrap();
-            assert_eq!(parse, ParseError { location: 7u });
+        fn generic_handler(raw: Box<Error>) {
+            let parse = raw.downcast::<ParseError>().unwrap();
+            assert_eq!(*parse, ParseError { location: 7u });
         }
 
         generic_handler(produce_parse_error())
